68530337b03597d5c19636a50b9315a44e4ff2e6
Liao_04 <31977310+liao04@users.noreply.github.com>
2019-11-23 21:09:35 -0800

    ''

--- bank/main/urls.py
+++ bank/main/urls.py
@@ -4,10 +4,17 @@ from django.urls import path
 from . import views
 from main.views import checking_acct_page
 
-
+#added by Kevin
 urlpatterns = [
 path("",views.index, name = "index"), #http response for home page
 path("main_page/",views.main_page,name = "main page"),
+path("promotion_1/",views.promotion_1,name ="promotion 1"),
+path("promotion_2/",views.promotion_2,name ="promotion 2"),
+path("promotion_3/",views.promotion_3,name ="promotion 3"),
+path("promotion_4/",views.promotion_4,name ="promotion 4"),
+path("promotion_5/",views.promotion_5,name ="promotion 5"),
+path("promotion_6/",views.promotion_6,name ="promotion 6"),
+path("manage_account/",views.manage_account,name = "manage account"),
 path("account_summary/",views.account_summary,name="account summary"),
 path("checking_acct_page/", checking_acct_page, name = "checking_acct_page") #added by rene
 ]
--- bank/main/views.py
+++ bank/main/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 
 from .models import checkingAcct
 
+
+######## added by Kevin ############
 def index(request):
     #HttpResponse takes html code as parameter
     return render(request,'main/login.html')
@@ -14,6 +16,29 @@ def main_page(request):
 
 def account_summary(request):
     return render(request,'main/account_summary.html')
+
+def promotion_1(request):
+    return render(request,'main/promotion1.html')
+
+def promotion_2(request):
+    return render(request,'main/promotion2.html')
+
+def promotion_3(request):
+    return render(request,'main/promotion3.html')
+
+def promotion_4(request):
+    return render(request,'main/promotion4.html')
+
+def promotion_5(request):
+    return render(request,'main/promotion5.html')
+
+def promotion_6(request):
+    return render(request,'main/promotion6.html')
+
+def manage_account(request):
+    return render(request,'main/manage_account.html')
+
+######added by Kevin ##########
 
 #added by Rene
 def checking_acct_page(request):
