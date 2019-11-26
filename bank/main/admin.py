1f758e04b6958f63925ada340e418ce570592cc4
Rene V <rene239@gmail.com>
2019-11-25 23:47:33 -0800

    made changes to admin.py bank/main/models.py

--- bank/main/admin.py
+++ bank/main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 #added by rene
+#from .models import User_Bank_ID
 from .models import Users
+from .models import Users_bank
 #from .models import Customers 
 from .models import Customers_List
 from .models import Accounts
@@ -10,7 +12,9 @@ from .models import checkingAcct
 
 
 #added by rene
+#admin.site.register(User_Bank_ID)
 admin.site.register(Users)
+admin.site.register(Users_bank)
 #admin.site.register(Customers)
 admin.site.register(Customers_List)
 admin.site.register(Accounts)
--- bank/main/models.py
+++ bank/main/models.py
@@ -1,16 +1,47 @@
 from django.db import models
 
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+#class User_Bank_ID(models.Model):
+ #   customer_id = models.AutoField(primary_key = True)
+  #  user = models.ForeignKey(User, on_delete = models.CASCADE)
+    #created_on = models.DateTimeField(auto_now = True, blank = True)
 
+#@receiver(post_save, sender = User)
+#def create_user_bank_id(sender, instance, created, **kwargs):
+ #   if created:
+  #      User_Bank_ID.objects.create(user=instance)
+
+#@receiver(post_save, sender = User)
+#def save_user_bank_id(sender, instance, **kwargs):
+    #instance.user_bank_id.save()
+
+
+class Users_bank(models.Model):
+    customer_id = models.AutoField(primary_key = True)  
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+   # username = models.CharField(max_length = 20, blank = True)
+   # username = models.ForeignKey('auth_user', on_delete=models.CASCADE)
+   # username = models.CharField(unique = True, max_length = 50)
+    password = models.CharField(max_length = 20, blank = True)
+    created_on = models.DateTimeField(auto_now = True, blank = True)
+
+@receiver(post_save, sender = User)
+def create_bank_customerID(sender, instance, created, **kwargs):
+   if created:
+       Users_bank.objects.create(username = instance)
 
 class Users(models.Model):
-    customer_id = models.AutoField(primary_key = True)
+    customer_id = models.AutoField(primary_key = True)  
     username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-   # username = models.CharField(unique = True, max_length = 50)
-    password = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20, blank = True)
     created_on = models.DateTimeField(auto_now = True, blank = True)
+
 
 class Customers(models.Model):
     customer_id = models.IntegerField(primary_key = True)
@@ -25,7 +56,7 @@ class Customers(models.Model):
 
 class Customers_List(models.Model):
     #customer_id = models.IntegerField(primary_key = True)
-    customer_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Users_bank, on_delete=models.CASCADE)
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     middle_name = models.CharField(max_length = 50)
@@ -53,5 +84,9 @@ class checkingAcct(models.Model):
     
 
 
+        
     
-    
+#@receiver(post_save, sender = Users_bank)
+#def create_checking_account(sender, created, **kwargs):
+   # if created:
+     #   Accounts.objects.create(account_type='checking')
